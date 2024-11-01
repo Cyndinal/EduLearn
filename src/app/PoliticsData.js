31af01f1735e30c1385bab2c1a898755cfcd5d0e
Max Cydin <nyarkomaxwell03@gmail.com>
2024-11-01 09:26:26 -0600

    New

---
+++ src/app/PoliticsData.js
@@ -0,0 +1,15 @@
+export const DataIntense = [
+    {
+        ImageUrl: "/Healthy.jpeg",
+        Description: "The Alberta Adult Health Benefit program covers health\n" +
+            "                    benefits for Albertans in low-income households who are pregnant or\n" +
+            "                    have high ongoing prescription drug needs. This health plan includes children\n" +
+            "                    who are 18 or 19 years old if they are living at home and attending high school.",
+    },
+      {
+        ImageUrl: "/Healthy.jpeg",
+        Description: "If you or other household members have coverage through another health benefits plan, you must use that plan first. The Alberta Adult Health Benefit plan may cover your remaining costs." +
+            "Talk to your dental provider, optical provider or pharmacist about how this works.",
+    },
+
+]
--- src/app/posts/[id]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function NotFound() {
-    return (
-        <div>Sorry post of id not found </div>
-    );
-}
-
-export default NotFound;
